5fadcfcb5171af71d5bcbefecb18ba5d77aed1e2
Callan Morrison <callan.l.morrison@gmail.com>
2016-02-13 10:08:42 -0600

    added copy to product array

--- products.js
+++ products.js
@@ -1,86 +1,86 @@
 var toys = [
 product1 = {
   name: "Sea Monkeys",
-  description: "description goes here",
+  description: "They breathe through their feet.",
   url: "img/product1.jpg",
   price: "$44.57",
   pageRef: "seaMonkeys"
 },
 product2 = {
   name: "Lunch Boxes",
-  description: "description goes here",
+  description: "Must use for actual food.",
   url: "img/product2.jpg",
   price: "$78.99",
   pageRef: "lunchBoxes"
 },
 product3 = {
   name: "Board Games",
-  description: "description goes here",
+  description: "In case you can't get a date IRL.",
   url: "img/product3.jpg",
-  price: "price goes here",
+  price: "$69.98",
   pageRef: "boardGames"
 },
 product4 = {
   name: "Tang",
   description: "It's what the astronauts drink!",
   url: "img/product4.jpg",
-  price: "price goes here",
+  price: "$17.96",
   pageRef: "tang"
 },
 product5 = {
   name: "Rock 'Em, Sock 'Em Robots",
-  description: "description goes here",
+  description: "Anger mgmt for kids.",
   url: "img/product5.jpg",
-  price: "price goes here",
+  price: "$260.00",
   pageRef: "rockEmSockEmRobots"
 },
 product6 = {
-  name: "Evil Knievel",
-  description: "description goes here",
+  name: "Evel Knievel",
+  description: "He wanted to marry his girlfriend, so he kidnapped her.",
   url: "img/product6.jpg",
-  price: "price goes here",
+  price: "$374.99",
   pageRef: "evilKnievel"
 },
 product7 = {
   name: "Atari",
-  description: "description goes here",
+  description: "Japanese 'ataru'= 'prepare to get your ass kicked'.",
   url: "img/product7.jpg",
-  price: "price goes here",
+  price: "$79.99",
   pageRef: "attari"
 },
 product8 = {
   name: "Teddy Ruxpin",
-  description: "description goes here",
+  description: "Official Spokesbear of Missing & Exploited kids",
   url: "img/product8.jpg",
-  price: "price goes here",
+  price: "$450.00",
   pageRef: "teddyRuxpin"
 },
 product9 = {
   name: "Stretch Armstrong",
-  description: "description goes here",
+  description: "Creators once drunkenly modified mold to have penis.",
   url: "img/product9.jpg",
-  price: "price goes here",
+  price: "$2,249.00",
   pageRef: "stretchArmstrong"
 },
 product10 = {
   name: "View Master",
-  description: "description goes here",
+  description: "Cool enough for Mary J. Blige.",
   url: "img/product10.jpg",
-  price: "price goes here",
+  price: "$127.39",
   pageRef: "viewMaster"
 },
 product11 = {
   name: "Moon Shoes",
-  description: "description goes here",
+  description: "Increases your insurance rate.",
   url: "img/product11.jpg",
-  price: "price goes here",
+  price: "$85.49",
   pageRef: "moonShoes"
 },
 product12 = {
   name: "Voltron",
-  description: "description goes here",
+  description: "Voltron: Defender of the Universe!",
   url: "img/product12.jpg",
-  price: "price goes here",
+  price: "$63.99",
   pageRef: "voltron"
 }
 ];
